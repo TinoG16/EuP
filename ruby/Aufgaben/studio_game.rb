c45ac469d7bddabd01eac0580735497caf6b842c
TinoG16 <tinoshandy@gmail.com>
2015-09-04 11:55:47 +0200

    erste schritte mit ruby

---
+++ ruby/Aufgaben/studio_game.rb
@@ -0,0 +1,14 @@
+name1 = 'larry'
+name2 = 'Curly'
+name3 = 'Moe'
+health = 60
+
+puts name1 +" s health is " + health.to_s
+
+puts "#{name1}'s health is #{health*3}"
+
+puts "#{name1}'s health is #{health/9}"
+
+puts "players: \n\tLarry\n\tCurly\n\tmoe"
+
+puts "players: \n\t#{name1}\n\t#{name2}\n\t#{name3}"
---
+++ ruby/code/start.rb
@@ -0,0 +1,15 @@
+str ="die woche besteht aus:\n\tMontag\n\tDienstag"
+
+puts str
+
+zahl = 15
+puts zahl	#puts konvertiert eine zahl in einem String, besser ist aber eine explizierte Konvertierung 
+
+#Zusammengesetzter String:
+puts str + zahl.to_s
+
+puts "#{str} der #{zahl}. im Monat"
+
+3. times do 
+	puts str
+end	
