5b7f99d8678560f61b6780e126d4b80f9a2cb35c
TinoG16 <tinoshandy@gmail.com>
2015-09-10 08:31:04 +0200

    Update start.rb

--- ruby/code/start.rb
+++ ruby/code/start.rb
@@ -1,6 +1,6 @@
 str ="die woche besteht aus:\n\tMontag\n\tDienstag"   variablen zuweisung mit zeichenkette (steuerzeichen n neue zeile t tab)
 
-puts str			#puts gibt was aus
+puts str			#puts erzeugt eine ausgabe auf der console
 
 zahl = 15
 puts zahl	#puts konvertiert eine zahl in einem String, besser ist aber eine explizierte Konvertierung 
