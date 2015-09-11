35cb3eb3a1d3db4f10d9903ca2dff05322414189
TinoG16 <tinoshandy@gmail.com>
2015-09-11 09:26:47 +0200

    was ein kampf

--- ruby/flicks.rb
+++ ruby/flicks.rb
@@ -1,5 +1,22 @@
-def movie_listing
-	puts "Ninja Turtles"
-end
+def movie_listing(title, rank=10)
+	"Movie: #{title.downcase.capitalize} hat #{Time.now.strftime("%A")}das Ranking #{rank}"
+end	
 
-movie_listing	
+def weekday
+
+end
+puts movie_listing("Ninja Turtles,6")	
+puts movie_listing("SWAT,9")
+
+
+# Beispiel für Variablen innerhalb von methoden
+
+# def movie_listing
+# 	var1 = " Movie: Ninja Turtles"
+# 	var2 = "Movie: quatsch mit sauce"		
+# 	"#{var1}\n#{var2}"
+# end
+
+# var = movie_listing
+# puts "var ausserhalb: #{var.objekt_id}"
+# puts "Rückgabewert #{var}"
